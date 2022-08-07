7af9291f74eb8bbe4aabb1b89d928905e00f2dd8
thezxdev <thezxdev@gmail.com>
2022-08-07 15:08:09 -0700

    Creacion del componente CounterApp con propTypes

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { FirstApp } from './FirstApp';
+import { CounterApp } from './CounterApp';
+// import { FirstApp } from './FirstApp';
 // import { HelloWorldApp } from './HelloWorldApp';
 
 // Importar estilos globales
@@ -9,6 +10,7 @@ import './styles.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		{/* Paso de valores a un componente con props */}
-		<FirstApp subTitle={ 12 } />
+		{/* <FirstApp subTitle={ 12 } /> */}
+		<CounterApp value={ 3 } />
 	</React.StrictMode>
 )
