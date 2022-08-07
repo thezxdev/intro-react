b8964421fe3b87093cd1463b8b1c2ef95d20a6c5
thezxdev <thezxdev@gmail.com>
2022-08-07 15:47:56 -0700

    useState

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,10 +1,24 @@
 import PropTypes from 'prop-types';
 
+// Cuando la funcion no hace uso de ningun valor dentro del componente es recomendable ponerlo afuera del mismo.
+// const handleAdd = ( e ) => {
+//   console.log( e );
+// }
+
 export const CounterApp = ({ value }) => {
+
+  const handleAdd = () => {
+    value = 1000;
+  }
+
   return (
     <>
       <h1>CounterApp</h1>
       <h2> { value } </h2>
+
+      <button onClick={ handleAdd }>
+        + 1
+      </button>
     </>
   )
 }
