bc8438a5e958f6f6eb08ddb869bbc6533024d16d
thezxdev <thezxdev@gmail.com>
2022-08-06 23:53:25 -0700

    utilizar estilos en react

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { FirstApp } from './FirstApp';
 // import { HelloWorldApp } from './HelloWorldApp';
 
+// Importar estilos globales
+import './styles.css';
+
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
         <FirstApp />
