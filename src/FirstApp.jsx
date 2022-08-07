7aaf1bbe843d6bb9bb3ac50a46915941694bef76
thezxdev <thezxdev@gmail.com>
2022-08-06 23:49:23 -0700

    variables en componentes

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,10 +1,26 @@
 
+const newMessage = {
+	message: 'Hola mundo',
+	title: 'jsjsj'
+};
+
+const getValue = () => {
+	return 'sssss';
+}
+
 export const FirstApp = () => {
+	
+	// Si no se cambia el valor o si no tiene una relacion con algun hook no se recomienda tener valores dentro de los componentes
+	// const newMessage = 'zxzx';
+
   return (
     // <> </> Fragment | Envoltorio, similar a un div pero sin poder usar estilos
+
+
     <>
-        <h1>FirstApp</h1>
-        <p>Soy un subtitulo</p>
+			<h1>{ getValue() }</h1>
+      {/* <code>{ JSON.stringify( newMessage ) }</code> */}
+      <p>Soy un subtitulo</p>
     </>
   );
 }
